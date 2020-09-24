e4e7a97e1c6c75a49b307307e91d37f70b284312
adriaanboon <71772080+adriaanboon@users.noreply.github.com>
2020-09-24 17:32:00 +0200

    Programmeeropdracht tweede dag met commentaar.

--- src/GebruikersInvoer.java
+++ src/GebruikersInvoer.java
@@ -4,15 +4,19 @@ public class GebruikersInvoer {
 
     public static void main(String[] args) {
 
+        //de variabele keyboard variabele bevat invoer van het toetsenbord.
+
         var keyboard = new Scanner(System.in);
 
         System.out.println("Voer een woord in, dan ziet u hoeveel letters dit woord heeft.");
 
+        //ontvang het ingevoerde woord, daarna wordt het aantal letters geteld.
         String invoerWoord = keyboard.nextLine();
         System.out.println(invoerWoord.length());
 
         System.out.println("Voer een getal in, dan wordt de tafel van dat getal uitgerekend!");
 
+        //vang het ingevoerde getal af.
         int invoerGetal=keyboard.nextInt();
 
         for (int teller = 1; teller <= 10; teller++) {
