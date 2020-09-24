aba97de50e7b8c3aaaf998f87f1ccc5d58cdf319
adriaanboon <71772080+adriaanboon@users.noreply.github.com>
2020-09-24 12:52:01 +0200

    Gebruikers invoer getal, dan tafel laten zien.

--- src/GebruikersInvoer.java
+++ src/GebruikersInvoer.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class GebruikersInvoer {
+
+    public static void main(String[] args) {
+
+
+        var keyboard = new Scanner(System.in);
+
+        System.out.println("Voer een getal in aub, dan reken ik de tafel van dat getal uit!");
+
+        int invoerGetal=keyboard.nextInt();
+
+        for (int teller = 1; teller <= 10; teller++) {
+            int tafel = teller * invoerGetal;
+
+            System.out.print(teller);
+            System.out.print(" x " + invoerGetal + " = ");
+            System.out.println(tafel);
+
+        }
+    }
+
 }
